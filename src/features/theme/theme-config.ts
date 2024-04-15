cab0baf410d774bfd29a034ac484c4ec6fa8435b
DB19222 <danny.avdberg@gmail.com>
2024-04-15 09:39:50 +0200

    home screen text adjustments

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,8 +1,8 @@
-export const AI_NAME = "Azure Chat";
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
+export const AI_NAME = "UvA AI Chat";
+export const AI_DESCRIPTION = "Je neemt deel aan experimentele inzet van ChatGPT binnen het AC. Let er op dat je geen persoonsgegevens deelt. In het experiment wordt gekeken naar gebruik, verbruik en  monitoring en alerting opties. Deze dienst is nog niet productie klaar en kan haperingen vertonen. Reken er dus niet op dat je chatgeschiedenis bewaard blijft!";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are ChatGPT, a large language model trained by OpenAI, based on the GPT-4 architecture. You must always return in markdown format.
 
 You have access to the following functions:
 1. create_img: You must only use the function create_img if the user asks you to create an image.`;
