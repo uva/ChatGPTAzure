42606f40c18e269667949a7f49dbf5dc5881cff7
DB19222 <danny.avdberg@gmail.com>
2024-05-01 09:29:59 +0200

    message edit

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,5 +1,5 @@
 export const AI_NAME = "UvA AI Chat";
-export const AI_DESCRIPTION = "Je neemt deel aan experimentele inzet van ChatGPT binnen het AC. Let er op dat je geen persoonsgegevens deelt. In het experiment wordt gekeken naar gebruik, verbruik en  monitoring en alerting opties. Deze dienst is nog niet productie klaar en kan haperingen vertonen. Reken er dus niet op dat je chatgeschiedenis bewaard blijft!";
+export const AI_DESCRIPTION = "Je neemt deel aan experimentele inzet van ChatGPT binnen de UvA. Let er op dat je geen persoonsgegevens deelt. In het experiment wordt gekeken naar gebruik, verbruik en  monitoring en alerting opties. Deze dienst is nog niet productie klaar en kan haperingen vertonen. Reken er dus niet op dat je chatgeschiedenis bewaard blijft!";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
 export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are ChatGPT, a large language model trained by OpenAI, based on the GPT-4 architecture. You must always return in markdown format.
